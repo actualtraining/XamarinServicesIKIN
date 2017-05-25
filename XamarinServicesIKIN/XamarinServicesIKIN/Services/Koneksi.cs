4903ea5256505681891d68ae7ef50ce330494341
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-05-25 10:52:02 +0700

    menambahkan services

---
+++ XamarinServicesIKIN/XamarinServicesIKIN/Services/Koneksi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XamarinServicesIKIN.Services
+{
+    public static class Koneksi
+    {
+        public static string RestUrl = "http://actservices.azurewebsites.net/";
+    }
+}
---
+++ XamarinServicesIKIN/XamarinServicesIKIN/Services/TodoItemServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using XamarinServicesIKIN.Model;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace XamarinServicesIKIN.Services
+{
+    public class TodoItemServices
+    {
+        private HttpClient _client;
+
+        public TodoItemServices()
+        {
+            _client = new HttpClient();
+        }
+        public async Task<List<TodoItem>> GetAllTodoItem()
+        {
+            var strUri = new Uri(Path.Combine(Koneksi.RestUrl, "api/TodoItem"));
+            var response = await _client.GetAsync(strUri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var data = JsonConvert.DeserializeObject<List<TodoItem>>(content);
+                return data;
+            }
+            else
+            {
+                throw new Exception("Error: " + response.StatusCode.ToString());
+            }
+        }
+    }
+}
