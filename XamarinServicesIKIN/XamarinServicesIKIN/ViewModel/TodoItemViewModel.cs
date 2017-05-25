24df29caef178ed033198d2460518a708e8c78cc
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-05-25 11:05:38 +0700

    buat method untuk ambil data

---
+++ XamarinServicesIKIN/XamarinServicesIKIN/ViewModel/TodoItemViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using XamarinServicesIKIN.Model;
+using XamarinServicesIKIN.Services;
+
+namespace XamarinServicesIKIN.ViewModel
+{
+    public class TodoItemViewModel : BindableObject
+    {
+        private TodoItemServices todoServices;
+        private List<TodoItem> listTodoItem;
+        public List<TodoItem> ListTodoItem
+        {
+            get { return listTodoItem; }
+            set { listTodoItem = value; OnPropertyChanged("ListTodoItem"); }
+        }
+
+        public async Task GetAllTodoItem()
+        {
+            listTodoItem = await todoServices.GetAllTodoItem();
+        }
+
+        public TodoItemViewModel()
+        {
+            todoServices = new TodoItemServices();
+        }
+
+    }
+}
